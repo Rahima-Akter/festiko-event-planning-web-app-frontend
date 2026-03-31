19ed4ab0f27857d7d4df3e052c75438fdd07ee9b
Rahima Akter <rahimaakter1527@gmail.com~>
2026-03-31 12:54:24 +0600

    added participation module actions with axios and backend api's

--- src/services/participation/participation.service.ts
+++ src/services/participation/participation.service.ts
@@ -1,12 +1,79 @@
 import { axiosActions } from "@/lib/axios/axiosHttp";
 
-export const inviteUser = async (eventId: string, userId:string) => {
+export const joinEvent = async (eventId: string) => {
   try {
-    const response = await axiosActions.axiosPost("participation/invite", {
-      data: {eventId, userId},
+    const response = await axiosActions.axiosPost("/participation/join", {
+      data: { eventId },
     });
     return response;
   } catch (err) {
     console.log(err);
+    throw err;
+  }
+};
+
+export const inviteUser = async (eventId: string, userId: string) => {
+  try {
+    const response = await axiosActions.axiosPost("participation/invite", {
+      data: { eventId, userId },
+    });
+    return response;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const exceptRejectInvite = async (id: string, status: string) => {
+  try {
+    const response = await axiosActions.axiosPatch(
+      `/participation/invitation/${id}`,
+      {
+        data: { id, status },
+      },
+    );
+    return response;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const approveRejectParticipation = async (
+  id: string,
+  status: string,
+) => {
+  try {
+    const response = await axiosActions.axiosPatch(`/participation/:id`, {
+      data: { id, status },
+    });
+    return response;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const getEventParticipants = async (eventId: string) => {
+  try {
+    const response = await axiosActions.axiosGet(
+      `/participation/participants/${eventId}`,
+    );
+    return response;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const getMyParticipations = async () => {
+  try {
+    const response = await axiosActions.axiosGet(
+      "/participation/my-participations",
+    );
+    return response;
+  } catch (err) {
+    console.log(err);
+    throw err;
   }
 };
